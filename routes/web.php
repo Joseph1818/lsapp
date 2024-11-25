0aa1c1460fd35078722aa8d90f92f4c40c00395f
umbaJ <joseph@dwebserver.net>
2024-11-25 12:14:36 +0200

    Added project

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/services', [PagesController::class, 'services']);
+ 
